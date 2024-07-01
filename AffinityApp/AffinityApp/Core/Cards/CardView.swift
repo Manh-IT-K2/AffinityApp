d1026579e1d21c50eafef377d3cd2d311da78659
Q. Mạnh <Quanmanh@192.168.17.12>
2024-07-01 09:32:08 +0700

    create card view

---
+++ AffinityApp/AffinityApp/Core/Cards/CardView.swift
@@ -0,0 +1,34 @@
+//
+//  CardViews.swift
+//  AffinityApp
+//
+//  Created by Q. Mạnh on 1/7/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    var body: some View {
+        ZStack(alignment: .bottom){
+            Image(.test1)
+                .resizable()
+                .scaledToFill()
+                .frame(width: cardWidth, height: cardHeight)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+    }
+}
+
+private extension CardView {
+    private var cardWidth: CGFloat {
+        UIScreen.main.bounds.width - 20
+    }
+    private var cardHeight: CGFloat {
+        UIScreen.main.bounds.height / 1.45
+    }
+}
+
+#Preview {
+    CardView()
+}
