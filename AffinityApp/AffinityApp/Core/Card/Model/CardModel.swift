65024e278ab3da13bbe36b603629bc5418527915
Qmanh <quanmanh901@gmail.com>
2024-07-03 13:36:37 +0700

    create cardModel

---
+++ AffinityApp/AffinityApp/Core/Card/Model/CardModel.swift
@@ -0,0 +1,18 @@
+//
+//  CardModel.swift
+//  AffinityApp
+//
+//  Created by Q. Mạnh on 3/7/24.
+//
+
+import Foundation
+
+struct CardModel {
+    let user: UserModel
+}
+
+extension CardModel: Identifiable {
+    var id: String {
+        return user.id
+    }
+}
